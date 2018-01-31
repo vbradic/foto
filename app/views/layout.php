f50fd7803b247f417819d0c372f0c64b1329b6ec
vbradic <76756B@gmail.com>
2018-01-31 01:39:17 +0100

    initial commit

---
+++ app/views/layout.php
@@ -0,0 +1,114 @@
+<html lang='en'>
+<head>
+<title>DieArtze</title>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+
+
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<link rel="stylesheet"
+	href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.cycle/3.0.3/jquery.cycle.all.js"></script> 
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/css/tether-theme-basic.min.css">
+
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+	rel="stylesheet">
+
+<link
+	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
+	type="text/css" rel="stylesheet" />
+
+<link href='https://fonts.googleapis.com/css?family=Bangers'
+	rel='stylesheet'>
+<link rel="stylesheet" type="text/css" href="app/css/header.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      
+      <div class="row">
+      <div class="col-xs-4 col-xs-offset-4 div_image">
+      	<img id="logo_img" class="img-responsive"  src="app/imgs/logo_small.jpg">
+      </div>
+      </div>
+     
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a class="hvr-underline-from-center" href="#">Link 1</a></li>
+        <li><a class="hvr-underline-from-center" href="#">Link 2</a></li>
+        
+        <!--  
+        <li class="dropdown">
+          <a class="hvr-underline-from-center" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a class="hvr-shutter-out-horizontal" href="#">Action</a></li>
+            <li><a class="hvr-shutter-out-horizontal" href="#">Another action</a></li>
+            <li><a class="hvr-shutter-out-horizontal" href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a class="hvr-shutter-out-horizontal" href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a class="hvr-shutter-out-horizontal" href="#">One more separated link</a></li>
+          </ul>
+        </li>
+        -->
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        
+        <li class="dropdown">
+          <a class="hvr-underline-from-center" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a class="hvr-shutter-out-horizontal" href="#">Action</a></li>
+            <li><a class="hvr-shutter-out-horizontal" href="#">Another action</a></li>
+            <li><a class="hvr-shutter-out-horizontal" href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a class="hvr-shutter-out-horizontal" href="#">Separated link</a></li>
+          </ul>
+        </li>
+        
+        <li><a class="hvr-underline-from-center" href="#">Link 3</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="row">
+<div class="col-xs-10 col-xs-offset-1">
+<div id="show-case">
+    <img class="img" src="app/imgs/red.jpg">
+    <img class="img" src="app/imgs/blue.jpg">
+    <img class="img" src="app/imgs/green.jpg">
+</div>    
+</div>
+</div>
+
+
+<script src="app/scripts/header.js"></script>
+
+</body>
+</html>
