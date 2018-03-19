802cbe997db92cbf6cac921b1a918414736748e2
vbradic <34651083+vbradic@users.noreply.github.com>
2018-03-19 23:23:03 +0100

    Add files via upload

--- app/views/video_gallery.php
+++ app/views/video_gallery.php
@@ -1,9 +1,13 @@
 <div class="row vide_gallery">
 	<div class="col-xs-12 col-md-6 col-md-offset-3">
+	<?php foreach($video_list as $video) { ?>
 		<div class="embed-responsive embed-responsive-16by9">
-		<iframe src="//player.vimeo.com/video/162170636"></iframe>
-	</div>
+			<iframe src="<?php echo $video->video_path;?>" ></iframe>
+		</div>
+		<br>
+	<?php }?>
 	</div>
 	
+
 </div>
 
